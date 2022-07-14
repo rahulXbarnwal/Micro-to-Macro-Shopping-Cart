c400f9c4e9c00892d3c11dc882a2683fcca0cdf4
RAHUL <rahulbarnwalofficial@gmail.com>
2022-07-14 12:08:17 +0530

    App Updated

--- functions/users.js
+++
@@ -1,8 +0,0 @@
-exports.handler = (event, context, callback) => {
-	callback(null, {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: `Hello world ${Math.floor(Math.random() * 10)}`,
-		}),
-	});
-};
